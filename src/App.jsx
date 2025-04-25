328932a4ae431971efb5b0cb1a3bfd4844a3ed93
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2025-04-25 14:17:39 +0200

    Create frontend skeleton with added packages and dependencies. Also added temporary .gitkeep files to folders that were empty to enable them to be commited.

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-green-500">Test: remove when needed</h1> // Just here to test if everything is installed correctly. Remove when other content is added to the web page. 
+  )
+}
+
+export default App

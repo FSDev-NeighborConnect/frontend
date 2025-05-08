791c88615b44fec85ced244dc4e7d77a6b638964
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2025-05-08 18:11:56 +0200

    Reformatted some of the tailwind code to be easier to read and maintain.

--- src/pages/NewUser/newuser.jsx
+++ src/pages/NewUser/newuser.jsx
@@ -204,7 +204,8 @@ function NewUser() {
                   <button
                     type="button"
                     onClick={() => setShowHobbiesModal(true)}
-                    className="mt-1 w-full flex justify-between items-center px-3 py-2 bg-purple-50 border border-gray-300 text-gray-800 rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
+                    className="mt-1 w-full flex justify-between items-center px-3 py-2 bg-purple-50 border border-gray-300 
+                    text-gray-800 rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
                   >
                     <span>
                       {formData.hobbies.length > 0 
@@ -227,7 +228,9 @@ function NewUser() {
             <div className="mt-8">
               <button
                 type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 font-roboto"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm 
+                font-medium text-white bg-purple-700 hover:bg-purple-800 focus:outline-none 
+                focus:ring-2 focus:ring-offset-2 focus:ring-purple-600 font-roboto"
               >
                 Register
               </button>
@@ -250,7 +253,8 @@ function NewUser() {
               <div>
                 <Link
                   to="/login"
-                  className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 font-roboto"
+                  className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm 
+                  font-medium text-gray-700 bg-white hover:bg-gray-50 font-roboto"
                 >
                   Sign in
                 </Link>

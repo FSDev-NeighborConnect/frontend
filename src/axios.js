69326d059f1c58028fb754e438468f59afc86308
reemallicious <reemsoud1993@gmail.com>
2025-05-12 12:06:07 +0200

    update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // Create an axios instance with a base URL
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Adjust this to match your backend URL
+  baseURL: "https://backend-7lde.onrender.com/", // Adjust this to match your backend URL
   headers: {
     "Content-Type": "application/json",
   },
